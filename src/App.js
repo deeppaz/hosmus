1bd60220e5add0beb967caae9e765ade01f329dd
deeppaz <deeppaz@hotmail.com>
2020-06-20 15:23:27 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import styled from 'styled-components';
+
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/photos").then((response) => response.json()).then((photos) => console.log(photos))
+  }, [])
+  const [images, setImages] = useState("hello world");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <h1>image gallery</h1>
+    </StyledApp>
   );
 }
+
+const StyledApp = styled.div`
+background: blue;
+font-size: 18px;
+padding: 30px;
+text-align: center;
+`
+
 
 export default App;

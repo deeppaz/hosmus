559602159519ed35594e9ae30ecb59adefc33bcb
deeppaz <deeppaz@hotmail.com>
2020-06-20 16:12:42 +0300

    update

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>search bar</h1>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;

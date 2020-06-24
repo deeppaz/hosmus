9a272fddfbce20976e6303d0a28dc887eb2fd599
deeppaz <deeppaz@hotmail.com>
2020-06-24 23:31:51 +0300

    update

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { Component } from "react";
+const omit = (obj, omitKey) =>
+	Object.keys(obj).reduce((result, key) => {
+		if (key !== omitKey) {
+			result[key] = obj[key];
+		}
+		return result;
+	}, {});
 
-class Loader extends React.Component {
-  constructor(props) {
-    super(props);
-    this.enableMessage = this.enableMessage.bind(this);
+const overlayStyles = {
+	position: "absolute",
+	filter: "blur(1px)",
+	transition: "opacity ease-in 1000ms",
+	clipPath: "inset(0)"
+};
 
-    this.state = {
-      displayMessage: false,
-    };
-
-    this.timer = setTimeout(this.enableMessage, 250);
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timer);
-  }
-
-  enableMessage() {
-    this.setState({displayMessage: true});
-  }
-
-  render() {
-    const {displayMessage} = this.state;
-
-    if (!displayMessage) {
-      return null;
-    }
-
-    return <div>Loading...</div>;
-  }
+export default class ProgressiveImage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { highResImageLoaded: false };
+	}
+	render() {
+		const { overlaySrc } = this.props;
+		const { highResImageLoaded } = this.state;
+		let filteredProps = omit(this.props, "overlaySrc");
+		return (
+			<span>
+				<img
+					{...filteredProps}
+					onLoad={() => {
+						this.setState({ highResImageLoaded: true });
+					}}
+					ref={img => {
+						this.highResImage = img;
+					}}
+					src={this.props.src}
+				/>
+				<img
+					{...filteredProps}
+					className={`${this.props.className} ${overlayStyles}`}
+					{...highResImageLoaded && { style: { opacity: "0" } }}
+					src={overlaySrc}
+				/>
+			</span>
+		);
+	}
 }
-
-export default Loader;

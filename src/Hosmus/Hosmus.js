a3fa48a138773c5c1e7f461deca57a8a227bd829
deeppaz <deeppaz@hotmail.com>
2020-06-28 18:21:43 +0300

    update

--- src/Hosmus/Hosmus.js
+++ src/Hosmus/Hosmus.js
@@ -8,7 +8,8 @@ import Skeleton from '../Skeleton/Skeleton';
 
 const credentials = {
     // id: 'EEEgGplDzD_rhmIum7skhws-rfIQKW9_4ULGSUqj1vE',
-    id: 'b7ffe8e3efba93372c464cc90497f32a3825d4fe38d4154d53a5000da26a0f5e',
+    // id: 'b7ffe8e3efba93372c464cc90497f32a3825d4fe38d4154d53a5000da26a0f5e',
+    id: 'BRBXlLy_41GUs3bkC8Q6wToRBDzotZF640DQB2YjvMA',
     base_url: 'https://api.unsplash.com/photos/random'
 }
 
